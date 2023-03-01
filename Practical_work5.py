b0fffe95f3c75f8bb78dfdccda43b4ba49b66836
Stepan <Smirnov-stepan2004@mail.ru>
2023-03-01 22:38:54 +0300

    Add changes

--- Practical_work5.py
+++ Practical_work5.py
@@ -1,14 +1,22 @@
 class People:
-    def __init__(self, name, age, direction):
+    __people = []
+
+    def __init__(self, name=input(), age=int(input()), orientation=input()):
         self.__name = name
         self.__age = age
-        self.__direction = direction
+        self.__orientation = orientation
 
     def __setattr__(self, key, value):
-        if key in ['_People__name', '_People__age', '_People__direction']:
-            self.__dict__[key] = value
+        if key in ['_People__name', '_People__age', '_People__orientation']:
+            if key == '_People__orientation' and value.lower() not in ['men', 'women']:
+                self.__dict__[key] = value
+            elif key == '_People__age' and isinstance(value, int) and 0 <= value <= 100:
+                self.__dict__[key] = value
+            else:
+                ValueError('Invalid value')
         else:
             raise AttributeError('You cannot create new attributes')
+        People.__people.append(self.__name)
 
     def change_user(self):
         __user = input('Enter the number:\n1 - change name\n2 - change age\n3-change direction')
@@ -16,7 +24,20 @@ class People:
             self.__name = input('Enter the name: ')
         elif __user == 2:
             self.__age = input('Enter the age: ')
-        elif __user == 3:
-            self.__direction = input('Enter the direction: ')
         else:
-            raise 
+            raise ValueError('Invalid value')
+
+    def __add__(self, other):
+        if self.__orientation != other.__orientation:
+            People()
+
+    def delete_user(self):
+        print('User was delete')
+        People.__people.remove(self.__name)
+
+    @staticmethod
+    def show_people():
+        print(*People.__people)
+
+
+user1 = People('Stepa', 18, )
