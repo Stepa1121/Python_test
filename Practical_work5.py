85a8121bfab0e1d9a48a0290d9d694784eb4626e
Stepan <Smirnov-stepan2004@mail.ru>
2023-03-02 23:31:19 +0300

    Add file

--- Practical_work5.py
+++ Practical_work5.py
@@ -1,24 +1,27 @@
 class People:
-    __people = []
+    __people = dict()
 
-    def __init__(self, name=input(), age=int(input()), orientation=input()):
+    def __init__(self, name, age, orientation):
         self.__name = name
         self.__age = age
         self.__orientation = orientation
+        People.__people[name] = self
 
-    def __setattr__(self, key, value):
+    def __setattr__(self, key, value):  # Function for checking user data
         if key in ['_People__name', '_People__age', '_People__orientation']:
             if key == '_People__orientation' and value.lower() not in ['men', 'women']:
-                self.__dict__[key] = value
+                if key == '_People__name' and value.isalpha():
+                    self.__dict__[key] = value
+                else:
+                    raise TypeError('Invalid value')
             elif key == '_People__age' and isinstance(value, int) and 0 <= value <= 100:
                 self.__dict__[key] = value
             else:
                 ValueError('Invalid value')
         else:
             raise AttributeError('You cannot create new attributes')
-        People.__people.append(self.__name)
 
-    def change_user(self):
+    def change_user(self):  # Function for changing user data
         __user = input('Enter the number:\n1 - change name\n2 - change age\n3-change direction')
         if __user == 1:
             self.__name = input('Enter the name: ')
@@ -27,17 +30,29 @@ class People:
         else:
             raise ValueError('Invalid value')
 
-    def __add__(self, other):
+    def __add__(self, other):  # Function for crossing users
         if self.__orientation != other.__orientation:
-            People()
+            create_person(age=0)
 
     def delete_user(self):
         print('User was delete')
-        People.__people.remove(self.__name)
+        del People.__people[self.__name]
 
     @staticmethod
     def show_people():
         print(*People.__people)
 
 
-user1 = People('Stepa', 18, )
+def create_person(name=input(), age=int(input()), orientation=input()):
+    People(name, age, orientation)
+
+
+user = int(input())
+while user == 1:
+    create_person()
+    user = int(input())
+
+
+
+
+
